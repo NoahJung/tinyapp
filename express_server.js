4f70e721754246ddb58477fbe3cf19324e00ad38
Minjae Jung <noahjung87@gmail.com>
2021-03-18 08:41:41 +0000

    Filled Out the templete

--- express_server.js
+++ express_server.js
@@ -17,10 +17,18 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+/* before apply ejs
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+*/
